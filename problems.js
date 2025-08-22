d8f56634366ba88a54f9c1b016146fd5800b7f4d
radin-111 <radinsheikh87@gmail.com>
2025-08-22 18:22:33 +0600

    adding solution of problem 4 and 5

--- problems.js
+++ problems.js
@@ -42,4 +42,43 @@ function checkPalindrome(word) {
   }
 }
 
+// Problem 4
 
+function findMaxNumber(numbers) {
+  let b = 0;
+  if (Array.isArray(numbers) === false) {
+    return "not array";
+  }
+
+  for (let num of numbers) {
+    if (typeof num !== "number") {
+      return "Invalid type of data";
+    }
+  }
+
+  for (let num of numbers) {
+    if (num > b) {
+      b = num;
+    }
+  }
+  return b;
+}
+
+// Problem 5
+function removeDuplicate(a) {
+  if (Array.isArray(a) === false) {
+    return "not array";
+  }
+
+  const b = [];
+
+  for (let i of a) {
+    if (b.includes(i) === false) {
+      b.push(i);
+    }
+  }
+  return b;
+}
+
+// const res = findMaxNumber([66,234,243,423,35453,19]);
+// console.log(res);
